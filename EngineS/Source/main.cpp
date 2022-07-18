df4dd7667762e004eb0024623a5b8797b1c622f6
Tr1pleS <shiss2001@outlook.com>
2022-07-18 22:32:20 +0800

    Make app runs an empty window

--- EngineS/Source/main.cpp
+++ EngineS/Source/main.cpp
@@ -1,10 +1,31 @@
-#include "Math/Math.hpp"
-#include "Math/Vector2.hpp"
-#include "Math/Vector3.hpp"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <memory>
+
+#include "Core/Base/Macros.hpp"
+#include "Engine.hpp"
+#include "Function/Global/Global.hpp"
+#include "Function/Input/InputSystem.hpp"
+#include "Function/Render/WindowSystem.hpp"
 
 using namespace EngineS;
 
+std::unique_ptr<Engine> engine;
+
+void Update(float deltaTime) {
+	if (Global::Instance()->inputSystem->GetKey(KeyCode::Escape)) {
+		engine->Shutdown();
+	}
+}
+
 int main() {
+	engine = std::make_unique<Engine>();
+	engine->StartEngine();
+	engine->Initialize();
+	engine->RegisterUpdateFunc(ENGINES_CALLBACK(Update));
+	engine->Run();
+
 	return 0;
 }
