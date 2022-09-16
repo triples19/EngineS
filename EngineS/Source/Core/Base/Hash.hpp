79af328992a0bd22ef131b34cbac8aaf62a628d1
Trip1eS <shiss2001@outlook.com>
2022-09-16 14:37:49 +0800

    Add HashCombine function

--- EngineS/Source/Core/Base/Hash.hpp
+++ EngineS/Source/Core/Base/Hash.hpp
@@ -11,3 +11,9 @@ template<>
 struct Hasher<fs::path> {
 	std::size_t operator()(const fs::path& path) const { return fs::hash_value(path); }
 };
+
+template<typename T, typename... Rest>
+void HashCombine(std::size_t& seed, const T& v, const Rest&... rest) {
+	seed ^= std::hash<T> {}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+	(HashCombine(seed, rest), ...);
+}
