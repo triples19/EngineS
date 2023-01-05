11c19ded5a24386a02412c8184592d39355cbe29
Tr1pleS <shiss2001@outlook.com>
2023-01-05 13:03:36 +0800

    Fix bustache.cmake path

--- ThirdParty/bustache.cmake
+++ ThirdParty/bustache.cmake
@@ -1,12 +1,12 @@
 add_library(
-  bustache
-  bustache/src/format.cpp
-  bustache/src/render.cpp
+        bustache
+        ${CMAKE_CURRENT_LIST_DIR}/bustache/src/format.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/bustache/src/render.cpp
 )
 
 target_compile_features(bustache PUBLIC cxx_std_20)
 
-target_include_directories(bustache PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/bustache/include)
+target_include_directories(bustache PUBLIC ${CMAKE_CURRENT_LIST_DIR}/bustache/include)
 
 target_link_libraries(bustache PUBLIC fmt::fmt)
 target_compile_definitions(bustache PUBLIC BUSTACHE_USE_FMT)
